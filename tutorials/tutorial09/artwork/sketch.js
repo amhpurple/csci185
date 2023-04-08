247a7809ccaed0c2e03970b8f1a834f3536b8b33
Alex <ahaney@gmail.com>
2023-04-07 22:57:04 -0400

    stuff

--- tutorials/tutorial09/artwork/sketch.js
+++ tutorials/tutorial09/artwork/sketch.js
@@ -16,14 +16,60 @@ function setup() {
     createCanvas(canvasWidth, canvasHeight);
     // frameRate(2);
     
-    fill("#08415c");
+    // fill("#08415c");
 
-    // draw a random circle:
-    let x = randomInt(0, canvasWidth);
-    let y = randomInt(0, canvasHeight);
-    let size = randomFloat(25, 125);
-    circle(x, y, size);
-    
+    let i = 0;
+    while  (i < 400) { 
+        // if (i % 7 === 3) {
+        //     fill("pink"); 
+        // }
+        // else if (i % 9 === 1) {
+        //     fill("orange");
+        // }
+        // else if (i % 12 === 0) {
+        //     fill("cyan");
+        // }
+        // else {
+        //     fill("lavender");
+        // }
+        fill (colors[randomInt(0,4)])
+        
+        if (i % 7 === 0) {
+            // draw a random square:
+            let x = randomInt(0, canvasWidth);
+            let y = randomInt(0, canvasHeight);
+            let size = randomFloat(25, 125);
+            square(x, y, size);
+        }
+        else if (i % 10 === 1) {
+            // draw a random triangle:
+            let x = randomInt(0, canvasWidth);
+            let y = randomInt(0, canvasHeight);
+            let size = randomFloat(25, 125);
+            triangle(
+                x, y - size / 2, 
+                x - size / 2.25, y + size / 4.3,
+                x + size / 2.25, y + size / 4.3
+            )
+        }
+        else if (i % 17 === 0) {
+            // draw a random line:
+            let x = randomInt(0, canvasWidth);
+            let y = randomInt(0, canvasHeight);
+            let size = randomFloat(25, 125);
+            let coefX = randomFloat(-3, 3);
+            let coefY = randomFloat(-3, 3);
+            line(x, y, x + size * coefX, y + size * coefY);
+        }
+        else {
+            // draw a random circle:
+            let x = randomInt(0, canvasWidth);
+            let y = randomInt(0, canvasHeight);
+            let size = randomFloat(25, 125);
+            circle(x, y, size);
+        }
+    ++i;
+    }
 }
 
 
--- tutorials/tutorial09/cars/sketch.js
+++ tutorials/tutorial09/cars/sketch.js
@@ -6,8 +6,32 @@ const c1 = {
     x: 100,
     y: 100,
     width: 200,
-    speed: 0.2,
-    color: 'hotpink'
+    speed: 4,
+    color: 'pink'
+};
+
+const c2 = {
+    x: 1200,
+    y: 300,
+    width: 200,
+    speed: 4,
+    color: 'orange'
+}
+
+const c3 = {
+    x: 100,
+    y: 200,
+    width: 100,
+    speed: 4,
+    color: 'lavender'
+};
+
+const t1 = {
+    x: 100,
+    y: 500,
+    width: 200,
+    speed: 4,
+    color: 'cyan'
 };
 
 
@@ -22,14 +46,44 @@ function draw() {
     // clear the canvas:
     clear();
 
+   
     // move the car:
     c1.x += c1.speed;
-
+    c1.speed = c1.speed + .05;
+    c2.x = c2.x - c2.speed;
+    c2.speed = c2.speed + .05;
+    c3.x = c3.x + c3.speed;
+    c3.speed = c3.speed + .1;
+    t1.x += t1.speed;
+    t1.speed = t1.speed + .02;
     // redraw the car:
     drawCar(c1.x, c1.y, c1.width, c1.color);
+    drawCar(c2.x, c2.y, c2.width, c2.color);
+    drawCar(c3.x, c3.y, c3.width, c3.color);
+    drawTruck(t1.x, t1.y, t1.width, t1.color);
+
+    if (c1.x > canvasWidth + 100) {
+        c1.x = -100;
+        c1.speed = 1;
+    } 
+    
+    if (c2.x < -100) {
+        c2.x = 1200;
+        c2.speed = 1;
+    }
+
+    if (c3.x > canvasWidth + 50) {
+        c3.x = -50;
+        c3.speed = 1;
+    }
+    
+    if (t1.x > canvasWidth + 100) {
+        t1.x = -100;
+        t1.speed = 1;
+    }
     
     // draw the grid (optional -- feel free to remove this line):
-    drawGrid(canvasWidth, canvasHeight);
+    // drawGrid(canvasWidth, canvasHeight);
 }
 
 
@@ -47,4 +101,19 @@ function drawCar(x, y, size, fillColor, wheelColor='black') {
     fill(wheelColor);
     circle(x - size / 4, y, size / 6);
     circle(x + size / 4, y, size / 6);
+}
+
+
+function drawTruck(x, y, size, fillColor, wheelColor='black') {
+    strokeWeight(0);
+
+    // body
+    fill(fillColor);
+    rect(x - size/7, y - (size/5 + size/7), size / 2, size/6); // top
+    rect(x - size/2, y - size/5, size, size/4); // bottom
+
+     // wheels:
+     fill(wheelColor);
+     circle(x - size / 4, y + 10, size / 4);
+     circle(x + size / 4, y + 10, size / 4);
 }
